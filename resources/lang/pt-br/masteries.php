57c804c6fd0f218027c05e62784e66b3400fc437
Hudson Pereira <hudson.byte@gmail.com>
2016-12-13 17:37:32 -0200

    adds help for masteries

--- resources/lang/pt-br/masteries.php
+++ resources/lang/pt-br/masteries.php
@@ -10,4 +10,14 @@ return [
     '7' => 'Aprendizagem',
     '8' => 'Auto defesa',
 
+    'help' => [
+        1 => 'Ajuda exclusivamente o dano do Cavaleiro.',
+        2 => 'Melhora a quantidade de vida, defesa e dano corporal.',
+        3 => 'Aumenta as recompensas quando uma masmorra é finalizada.',
+        4 => 'Aumenta o ouro recebido por abate, e exclusivamente o dano do Caçador.',
+        5 => 'Aumenta a resistência mágica, e exclusivamente o dano do Mago.',
+        6 => 'Aumenta o ouro recebido e a chance de acerto crítico.',
+        7 => 'Aumenta a experiência recebida por abate.',
+        8 => 'Melhora a defesa em qualquer tipo de combate.'
+    ]
 ];
