b0be6882cac3fa058d51bf4e8e52e2d9b352f0b8
Hudson Pereira <hudson.byte@gmail.com>
2017-03-19 14:24:55 -0300

    open graph hotfix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ config('app.locale') }}">
-  <head>
+  <head prefix="og: http://ogp.me/ns#">
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
